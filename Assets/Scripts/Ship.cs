c823b0c1b05aa7e32a89c830f036ab42fe338800
Pace Smith <pace@pacesmith.com>
2018-09-01 12:44:22 -0400

    compute the ship grid location corresponding to the mouse cursor, but don't do anything with it yet

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Ship : MonoBehaviour {
 
 	public static Ship instance;
-	Piece[,] m_pieces = new Piece[5, 3]; // @todo extend
+	Piece[,] m_pieces = new Piece[5, 3]; // (0,0) is the bottom-leftmost possible ship piece grid coordinate. @todo extend
     private Vector2Int gridCenter = new Vector2Int(2, 1);
+    private Vector2 bottomLeftCorner;
     public float pieceWidthInUnits;
     public float pieceHeightInUnits;
+    private Vector2 pieceSize;
     private Vector2Int reservedGridSlot = new Vector2Int(-1, -1); // (-1,-1) indicates no grid slot is currently reserved
 
     private MainController main;
@@ -32,9 +34,14 @@ public class Ship : MonoBehaviour {
 	{
         GameObject manager = GameObject.Find("GameManagers");
         main = manager.GetComponent<MainController>();
+
+        bottomLeftCorner = PiecePosition(0,0);
+        bottomLeftCorner.x -= pieceWidthInUnits / 2;
+        bottomLeftCorner.y -= pieceHeightInUnits / 2;
+        pieceSize = new Vector2(pieceWidthInUnits, pieceHeightInUnits);
         
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 3; j++) {
+        for(int i = 1; i < 4; i++) {
+            for(int j = 1; j < 2; j++) {
                 GameObject piecePrefab = main.RandomPiecePrefab();
                 Vector2 relativePosition = PiecePosition(i,j);
                 GameObject pieceObj = Instantiate(piecePrefab, relativePosition, Quaternion.identity, transform);
@@ -56,53 +63,60 @@ public class Ship : MonoBehaviour {
         return new Vector2(offsetx, offsety);
     }
 
-    // This is called when the player is done dragging, and drops a ship piece onto the edge of the ship.
+    // This is called when the player is done dragging, and drops a ship piece onto a valid ship grid location.
     // If we allow already-attached ship pieces to be reorganized, this will also get called on a single click: picking up a ship piece and putting it right back.
     // @return Did the piece successfully drop?
     public bool ReceivePieceFromMouse(Piece piece)
     {
         return false; // stub
     }
-    
+
+    // Note that a grid slot is currently being dragged from
     public void ReserveGridSlot(Vector3 worldPos3d)
     {
         reservedGridSlot = GridSlot(worldPos3d);
         //Debug.Log("Reserved grid slot " + reservedGridSlot);
     }
 
+    // Note that no grid slot is currently being dragged from
     public void ReleaseGridSlot()
     {
         reservedGridSlot = new Vector2Int(-1, -1);
     }
-    
-    public Vector2Int GridSlot(Vector3 worldPos3d)
-        // returns which (x,y) grid slot, with origin at bottom left, is the best fit for worldPos, or (-1, -1) if worldPos is outside the max ship bounds
-    {
-        return GridSlot(worldPos3d);
+
+    // my max width in units
+    public float MaxWidth() {
+        return pieceWidthInUnits * m_pieces.GetLength(0);
     }
+
+    // my max height in units
+    public float MaxHeight() {
+        return pieceHeightInUnits * m_pieces.GetLength(1);
+    }
+    
+    // returns which (x,y) grid slot, with origin at bottom left, is the best fit for worldPos, or (-1, -1) if worldPos is outside the max ship bounds
     public Vector2Int GridSlot(Vector2 worldPos) {
-        // returns which (x,y) grid slot, with origin at bottom left, is the best fit for worldPos, or (-1, -1) if worldPos is outside the max ship bounds
-        // Vector2 offset = worldPos - bottomLeftCorner;
-        // //Debug.Log("offset = " + offset + " slotsize = " + slotSize + " spriteSize = " + spriteSize);
-        // if (offset.x < 0 || offset.x > spriteSize.x
-        //     || offset.y < 0 || offset.y > spriteSize.y)
-        // {
-        //     return new Vector2Int(-1, -1); // outside the max ship bounds
-        // } else
-        // { // inside the max ship bounds
-        //     Vector2 floatyResult = offset / slotSize;
-        //     int resultX = (int)Math.Floor(floatyResult.x);
-        //     int resultY = (int)Math.Floor(floatyResult.y);
-        //     return new Vector2Int(resultX, resultY);
-        // }
+        Vector2 offset = worldPos - bottomLeftCorner;
+        //Debug.Log("offset = " + offset + " piecesize = " + pieceSize + " spriteSize = " + spriteSize);
+        if (offset.x < 0 || offset.x > MaxWidth()
+            || offset.y < 0 || offset.y > MaxHeight())
+        {
+            return new Vector2Int(-1, -1); // outside the max ship bounds
+        } else
+        { // inside the max ship bounds
+            Vector2 floatyResult = offset / pieceSize;
+            int resultX = (int)Math.Floor(floatyResult.x);
+            int resultY = (int)Math.Floor(floatyResult.y);
+            return new Vector2Int(resultX, resultY);
+        }
         return new Vector2Int(-1,-1); // stub
     }
 
     // private void SnapToGridSlot(GameObject piece, Vector2Int slot)
     // {
     //     Vector2 offsetToOrigin = -1 * spriteSize / 2;
-    //     Vector2 pieceSizeOffset = slotSize / 2;
-    //     Vector2 slotOffset = slot * slotSize;
+    //     Vector2 pieceSizeOffset = pieceSize / 2;
+    //     Vector2 slotOffset = slot * pieceSize;
     //     Vector2 offset = offsetToOrigin + slotOffset + pieceSizeOffset;
     //     //Debug.Log("SnapToGridSlot: " + offsetToOrigin + " " + slotOffset + " " + pieceSizeOffset);
     //     piece.transform.localPosition = ZeroZ(offset);
